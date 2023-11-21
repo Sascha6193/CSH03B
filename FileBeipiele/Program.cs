0a3601ebbcb9656b2f8897d0ffa293c420dd9e4b
Sascha <Saschabauer6193@icloud.com>
2023-11-21 09:33:43 +0100

    FileBeispiele fertig

--- FileBeipiele/Program.cs
+++ FileBeipiele/Program.cs
@@ -12,39 +12,52 @@ namespace FileBeipiele
         static void Main(string[] args)
         {
             Program test = new Program();
-            string pfad = @"D:\Entwicklung\CSH-Lehrgang\CSH03\FileBeipiele\bin\Musterdatei.txt";
-            //byte[] array = { 68, 97, 116, 101, 105 };
-            // test.DateiErstellen(pfad, array);
-            string content = @"Die Text wird Inhalt der Datei.
-Er enthählt Zeilenumbrüche und ein Pfadangabe:
-D:\Entwicklung\CSH-Lehrgang\CSH03\FileBeipiele\bin\Release";
-            test.WriterNutzen(pfad, content);
-            test.ReaderNutzen(pfad);
+            string path = @"D:\Entwicklung\CSH-Lehrgang\CSH03\FileBeipiele\Musterdatei2.bin";
+            int[] pos = { 1400, 3250, 280 };
+            test.BinaryWrite(path, pos);
+
+            test.BinaryRead(path);
 
 
+            #region Ausgeklammerter code 
+            //string pfad = @"D:\Entwicklung\CSH-Lehrgang\CSH03\FileBeipiele\Musterdatei.txt";
+            ////byte[] array = { 68, 97, 116, 101, 105 };
+            //// test.DateiErstellen(pfad, array);
+            //string content = @"Die Text wird Inhalt der Datei.
+            //Er enthählt Zeilenumbrüche und ein Pfadangabe:
+            //D:\Entwicklung\CSH-Lehrgang\CSH03\FileBeipiele\Musterdatei";
+            //test.WriterNutzen(pfad, content);
+            //test.ReaderNutzen(pfad);
+            #endregion
+
+            Console.ReadKey();
 
         }
 
-        public void DateiErstellen(string pfad, byte[] array) 
-        {
-            FileStream stream = File.Open(pfad,FileMode.Create);
-            stream.Write(array, 0, array.Length);
-            stream.Close();
-        }
 
-        public void DateiLesen(string pfad)
-        {
-            FileStream stream = File.Open(pfad,FileMode.Open);
-            byte[] array = new byte[stream.Length];
-            stream.Read(array, 0, (int)stream.Length); 
-            for (int i = 0; i < array.Length; i++) 
-            {
-                Console.Write((char)array[i]);
-            }
-            Console.WriteLine();
-            stream.Close();
-        }
+        #region Methoden zum Verständniss  
+        //public void DateiErstellen(string pfad, byte[] array) 
+        //{
+        //    FileStream stream = File.Open(pfad,FileMode.Create);
+        //    stream.Write(array, 0, array.Length);
+        //    stream.Close();
+        //}
 
+        //public void DateiLesen(string pfad)
+        //{
+        //    FileStream stream = File.Open(pfad,FileMode.Open);
+        //    byte[] array = new byte[stream.Length];
+        //    stream.Read(array, 0, (int)stream.Length); 
+        //    for (int i = 0; i < array.Length; i++) 
+        //    {
+        //        Console.Write((char)array[i]);
+        //    }
+        //    Console.WriteLine();
+        //    stream.Close();
+        //}
+        #endregion
+
+        #region Methoden fürs einfaches lesen und schreiben von der Stream Klassen 
         public void WriterNutzen(string pfad,string content) 
         {
             StreamWriter writer = new StreamWriter(File.Open(pfad,FileMode.Create)); 
@@ -64,6 +77,37 @@ D:\Entwicklung\CSH-Lehrgang\CSH03\FileBeipiele\bin\Release";
             //Console.WriteLine(reader.ReadLine()); Kann raus wird oben ersetzt in schleife 
             reader.Close();
         }
+        #endregion
 
+        #region Binary Methoden 
+        public void BinaryWrite(string path, int[] content)
+        {
+            BinaryWriter writer = new BinaryWriter(File.Open(path,FileMode.Create));
+            for (int i = 0; i < content.Length; i++)
+            {
+                writer.Write(content[i]);
+            }
+            writer.Close();
+        }
+
+        public void BinaryRead( string path) 
+        {
+            BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open));
+            bool goOn = true;
+            while (goOn)
+            {
+                try 
+                { 
+                    Console.Write("{0} ", reader.ReadInt32());
+                }
+                catch (EndOfStreamException)
+                {
+                    goOn = false;
+                }
+            }
+            reader.Close();
+            Console.WriteLine();
+        }
+        #endregion
     }
 }
